20125c4f87f3ac25d0eb2c63b7fac01d284a689c
LYT <1929596302@qq.com>
2024-01-18 11:52:15 +0800

    【更新】数据库自动创建（sql），配置文件修改

--- server/initialize/gorm_mysql.go
+++ server/initialize/gorm_mysql.go
@@ -1,8 +1,11 @@
 package initialize
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/lindocedskes/global"
 	"github.com/lindocedskes/initialize/internal"
+	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,6 +20,7 @@ func GormMysql() *gorm.DB {
 		DefaultStringSize:         191,     // string 类型字段的默认长度
 		SkipInitializeWithVersion: false,   // 根据版本自动配置
 	}
+	CreateDatabaseIfNotExist()
 	if db, err := gorm.Open(mysql.New(mysqlConfig), internal.Gorm.Config()); err != nil {
 		return nil
 	} else {
@@ -25,4 +29,27 @@ func GormMysql() *gorm.DB {
 		sqlDB.SetMaxOpenConns(m.MaxOpenConns)
 		return db
 	}
+}
+
+// 若数据库不存在，则创建数据库
+func CreateDatabaseIfNotExist() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/",
+		global.NBUCTF_CONFIG.Mysql.Username,
+		global.NBUCTF_CONFIG.Mysql.Password,
+		global.NBUCTF_CONFIG.Mysql.Host,
+		global.NBUCTF_CONFIG.Mysql.Port))
+
+	if err != nil {
+		global.GVA_LOG.Error("open database error", zap.Any("err", err))
+	}
+
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS " + global.NBUCTF_CONFIG.Mysql.Dbname)
+
+	fmt.Println("Host:", global.NBUCTF_CONFIG.Mysql.Host)
+	fmt.Println("Port:", global.NBUCTF_CONFIG.Mysql.Port)
+	if err != nil {
+		global.GVA_LOG.Error("create database error", zap.Any("err", err))
+	}
+
+	global.GVA_LOG.Info("成功新建数据库：" + global.NBUCTF_CONFIG.Mysql.Dbname)
 }
