54994b55178cd5ddc787e8c3438160efd489fe5a
LYT <1929596302@qq.com>
2024-04-11 10:14:48 +0800

    【基础路由跳转】

--- nbuctfVue3/src/App.vue
+++ nbuctfVue3/src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+// 在 Vue3 CompositionAPI 中
+// 1. 获取路由对象 router  useRouter
+//    const router = useRouter()
+// 2. 获取路由参数 route   useRoute
+//    const route = useRoute()
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const goList = () => {
+  router.push('/list')
+  console.log(router, route)
+}
+</script>
 
 <template>
   <div>app.vue</div>
+  <button @click="$router.push('/home')">跳首页</button>
+  <button @click="goList">跳列表页</button>
 </template>
 
 <style scoped></style>
--- nbuctfVue3/src/router/index.js
+++ nbuctfVue3/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // import.meta.env.BASE_URL 是路由的基准地址，默认是 ’/‘。vite.config.js  添加配置项  base: '/' 。
   routes: []
 })
 
